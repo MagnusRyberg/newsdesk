9e152f9191639efb58a931a1bb198663ee31fdbf
MagnusRyberg <46519170+MagnusRyberg@users.noreply.github.com>
2023-10-20 09:37:07 +0200

    Create rss-links.js

---
+++ rss-links.js
@@ -0,0 +1,107 @@
+const rssLinks = [
+    {
+        title:"Investopedia",
+        url:"https://www.investopedia.com/feedbuilder/feed/getfeed?feedName=rss_articles"
+    },
+    {
+        title: "BBC News",
+        url: "http://feeds.bbci.co.uk/news/world/rss.xml"
+    },
+    {
+        title: "The Guardian",
+        url: "https://www.theguardian.com/world/rss"
+    },
+    {
+        title: "CNN",
+        url: "http://rss.cnn.com/rss/cnn_topstories.rss"
+    },
+    {
+        title: "Reuters",
+        url: "http://feeds.reuters.com/reuters/topNews"
+    },
+    {
+        title: "New York Times",
+        url: "https://www.nytimes.com/svc/collections/v1/publish/https://www.nytimes.com/section/world/rss.xml"
+    },
+    {
+        title: "E-IR",
+        url: "https://www.e-ir.info/feed/"
+    },
+    {
+        title: "Reuters - Business",
+        url: "http://feeds.reuters.com/reuters/businessNews"
+    },
+    {
+        title: "The Washington Post",
+        url: "http://feeds.washingtonpost.com/rss/national"
+    },
+    {
+        title: "The Wall Street Journal",
+        url: "https://feeds.a.dj.com/rss/RSSMarketsMain.xml"
+    },
+    {
+        title: "Global Issues",
+        url: "https://www.globalissues.org/news/feed"
+    },
+    {
+        title: "Yahoo World News",
+        url: "https://www.yahoo.com/news/rss/world/"
+    },
+    {
+        title: "CNBC",
+        url: "https://www.cnbc.com/id/100727362/device/rss/rss.html"
+    },
+    {
+        title: "The Economist",
+        url: "https://www.economist.com/sections/science-technology/rss.xml"
+    },
+    {
+        title: "ABC News International",
+        url: "https://abcnews.go.com/abcnews/internationalheadlines"
+    },
+    {
+        title: "CBS News - Health",
+        url: "https://www.cbsnews.com/latest/rss/health"
+    },
+    {
+        title: "Science Daily",
+        url: "https://www.sciencedaily.com/rss/top.xml"
+    },
+    {
+        title: "USA Today - Politics",
+        url: "http://rssfeeds.usatoday.com/usatoday-NewsTopStories"
+    },
+    {
+        title: "TechRadar",
+        url: "https://www.techradar.com/rss"
+    },
+    {
+        title: "CNET - Reviews",
+        url: "https://www.cnet.com/rss/reviews/"
+    },
+    {
+        title: "BBC Sport",
+        url: "http://feeds.bbci.co.uk/sport/rss.xml"
+    },
+    {
+        title: "National Public Radio (NPR) - World",
+        url: "https://www.npr.org/rss/rss.php?id=1004"
+    },
+    {
+        title: "HuffPost - Entertainment",
+        url: "https://www.huffpost.com/entertainment/feed"
+    },
+    {
+        title: "The Hollywood Reporter",
+        url: "https://www.hollywoodreporter.com/rss/news"
+    },
+    {
+        title: "Food Network - Recipes",
+        url: "http://www.foodnetwork.com/recipes.xml"
+    },
+    {
+        title: "National Geographic - Environment",
+        url: "https://www.nationalgeographic.com/environment/rss/news"
+    }
+    // Add more RSS feed links as needed
+];
